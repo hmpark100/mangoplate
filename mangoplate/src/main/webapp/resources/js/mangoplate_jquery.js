39ec3ddfa1c22c9bb10fc2f3077a119b6f4cee79
TITUS PARK <TITUS PARK@DESKTOP-TJE2G5P>
2022-10-13 15:45:40 +0900

    mangoplate_jquery 수정 좀 했습니다(하명)

--- mangoplate/src/main/webapp/resources/js/mangoplate_jquery.js
+++ mangoplate/src/main/webapp/resources/js/mangoplate_jquery.js
@@ -1,6 +1,28 @@
 $(document).ready(function(){
 
 	/*********************
+		망고스토리 에디터 등록 폼
+	**********************/
+	$("#btnBoardStoryWrite").click(()=>{
+		if($("#editer").val() == ""){
+			alert("에디터를 입력해주세요");
+			$("#editer").focus();
+			return false;
+		}else if($("#stitle").val() == ""){
+			alert("제목을 입력해주세요");
+			$("#stitle").focus();
+			return false;
+		}else if($("#sstitle").val() == ""){
+			alert("소제목을 입력해주세요");
+			$("#sstitle").focus();
+			return false;
+		}else{
+			//서버전송
+			boardWriteForm.submit();
+		}
+	});
+
+	/*********************
 		로그인폼 체크 함수
 	**********************/
 	$("#btnLogin").click(()=>{
