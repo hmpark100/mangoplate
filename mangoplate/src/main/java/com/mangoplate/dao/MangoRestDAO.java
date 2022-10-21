da6e6c13d71a360910394bcbe520dadd6f3a856e
T.H.KIM <97826979+Chamomile93@users.noreply.github.com>
2022-10-21 15:24:04 +0900

    22.10.21 식당 등록 DAO(RestDAO)랑 리뷰 등록 DAO (ReviewDAO)구분

--- mangoplate/src/main/java/com/mangoplate/dao/MangoRestDAO.java
+++ mangoplate/src/main/java/com/mangoplate/dao/MangoRestDAO.java
@@ -1,38 +1,5 @@
 package com.mangoplate.dao;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.mangoplate.vo.MangoRestVO;
-
-
-public class MangoRestDAO extends DBConn{
+public class MangoRestDAO {
 	
-	/**
-	 * insert : 게시글 등록
-	 */
-	public int insert(MangoRestVO vo) {
-		int result = 0;
-		String sql = "insert into mango_review "
-				+ " values('b_'||sequ_cgv_board.nextval, ?)";
-		
-		try {
-			getPreparedStatement(sql);
-			pstmt.setString(1, vo.getVcontent());
-			
-			result = pstmt.executeUpdate();
-			
-			close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		
-		return result;
-	}
 }
---
+++ mangoplate/src/main/java/com/mangoplate/dao/MangoReviewDAO.java
@@ -0,0 +1,29 @@
+package com.mangoplate.dao;
+
+import com.mangoplate.vo.MangoRestVO;
+
+public class MangoReviewDAO extends DBConn{
+	
+	/**
+	 * insert : 게시글 등록
+	 */
+	public int insert(MangoRestVO vo) {
+		int result = 0;
+		String sql = "insert into mango_review "
+				+ " values('b_'||sequ_cgv_board.nextval, ?)";
+		
+		try {
+			getPreparedStatement(sql);
+			pstmt.setString(1, vo.getVcontent());
+			
+			result = pstmt.executeUpdate();
+			
+			close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		return result;
+	}
+}
