57dd36d3447dcd81f5bc263e7ee2481cf04e2177
T.H.KIM <97826979+Chamomile93@users.noreply.github.com>
2022-10-26 10:46:29 +0900

    22.10.26

--- mangoplate/src/main/java/com/team/service/ListServiceImpl.java
+++ mangoplate/src/main/java/com/team/service/ListServiceImpl.java
@@ -2,14 +2,18 @@ package com.team.service;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.mangoplate.dao.MangoListDAO;
 import com.mangoplate.vo.MangoRestVO;
 
 public class ListServiceImpl implements ListService{
-
+	
+	@Autowired
+	private MangoListDAO listDao;
+	
 	public ArrayList<MangoRestVO> getContent(String rcategory){
-		MangoListDAO dao = new MangoListDAO();
-		ArrayList<MangoRestVO> list = dao.select(rcategory);
+		ArrayList<MangoRestVO> list = listDao.select(rcategory);
 		
 		return list;
 	}
